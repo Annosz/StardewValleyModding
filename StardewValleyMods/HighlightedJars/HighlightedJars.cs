eb5fda1e2e2277b0a448f7606ca8f5307b212097
Annosz <tadamleanyfalu@gmail.com>
2020-09-05 15:47:33 +0200

    HighlightedJars - Correct positions

--- StardewValleyMods/HighlightedJars/HighlightedJars.cs
+++ StardewValleyMods/HighlightedJars/HighlightedJars.cs
@@ -32,17 +32,20 @@ namespace HighlightedJars
 
         private void Display_RenderedWorld(object sender, RenderedWorldEventArgs e)
         {
+            if (Game1.currentLocation == null)
+                return;
+
             var highlightableObjects = Game1.currentLocation.objects.Values.Where(o =>
-                   (Config.HighlightJars && (o as StardewValley.Object).parentSheetIndex == 15)
-                   || (Config.HighlightKegs && (o as StardewValley.Object).parentSheetIndex == 12)
-                   ).ToList();
+                ((Config.HighlightJars && (o as StardewValley.Object).parentSheetIndex == 15)
+                    || (Config.HighlightKegs && (o as StardewValley.Object).parentSheetIndex == 12))
+                && o.minutesUntilReady <= 0 && !o.readyForHarvest
+                ).ToList();
 
             foreach (var highlightableObject in highlightableObjects)
             {
-                Vector2 vector2 = (highlightableObject.isOn ? new Vector2(Math.Abs(highlightableObject.TileLocation.X - 5f), Math.Abs(highlightableObject.TileLocation.Y - 5f)) : Vector2.Zero) * 4f;
                 Vector2 local = Game1.GlobalToLocal(Game1.viewport, new Vector2((float)(highlightableObject.TileLocation.X * 64), (float)(Math.Abs(highlightableObject.TileLocation.Y * 64 - 64))));
-                Rectangle destinationRectangle = new Rectangle((int)((double)local.X + 32.0 - 8.0 - (double)vector2.X / 2.0), (int)((double)local.Y + 64.0 + 8.0 - (double)vector2.Y / 2.0), (int)(16.0 + (double)vector2.X), (int)(16.0 + (double)vector2.Y / 2.0));
-                e.SpriteBatch.Draw(Game1.emoteSpriteSheet, destinationRectangle, new Rectangle(exclamationEmote * 16 % Game1.emoteSpriteSheet.Width, exclamationEmote * 16 / Game1.emoteSpriteSheet.Width * 16, 16, 16), Color.White * 0.95f, 0.0f, Vector2.Zero, SpriteEffects.None, (float)((highlightableObject.TileLocation.Y + 1) * 64) / 10000f);
+                Rectangle destinationRectangle = new Rectangle((int)local.X + (highlightableObject.shakeTimer > 0 ? Game1.random.Next(-1, 2) : 0), (int)local.Y + (highlightableObject.shakeTimer > 0 ? Game1.random.Next(-1, 2) : 0) - 32, 64, 64);
+                e.SpriteBatch.Draw(Game1.emoteSpriteSheet, destinationRectangle, new Rectangle(exclamationEmote * 16 % Game1.emoteSpriteSheet.Width, exclamationEmote * 16 / Game1.emoteSpriteSheet.Width * 16, 16, 16), Color.White * 0.95f, 0.0f, Vector2.Zero, SpriteEffects.None, (float)((highlightableObject.TileLocation.Y - 1) * 64) / 10000f);
             }
         }
     }
